d805b78711debad43e10671285071d2fee43dfb4
chamanos <chamano.sr@gmail.com>
2023-08-02 15:41:55 +0200

    making the code dynamic

--- challengeB/script.js
+++ challengeB/script.js
@@ -1,12 +1,27 @@
-const span = document.getElementById('span');
 
-span.addEventListener('click', toggleMenu);
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
+
+
+// function toggleMenu(event) {
+//     const a  = event.replace("span_","span");
+//     const span = document.getElementById(a);
+  
+//         const menu =  span.closest(".parent").querySelector(".menu");
+
+//         if (menu.style.display === "block") {
+//             menu.style.display = "none";
+//         } else {
+//             menu.style.display = "block";
+//         }
+  
+// }
+function toggleMenu(span) {
+    const menu = span.parentElement.nextElementSibling; // Get the sibling menu element
+
     if (menu.style.display === "block") {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
     }
-
 }
+
+
